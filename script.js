424407eabe6763b9a8f5f5f390a8527677e4a3c7
Kushal Vardhan <vardhank140102@gmail.com>
2025-03-25 18:24:35 +0530

    css on active class

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
 const gridView = document.querySelector('#gridView')
 const listView = document.querySelector('#listView')
-// gridView.style.display = "none"
-// listView.style.display = "block"
-
-
+const booksContainer = document.querySelector('.booksContainer')
 const url = "https://api.freeapi.app/api/v1/public/books"
+
+
 function getData() {
     fetch(url)
         .then((response) => {
             return response.json()
         })
         .then((books) => {
-            for (let i = 0; i <= 9; i++) {
+            const arrLength = books.data.data.length
+            for (let i = 0; i < arrLength; i++) {
                 const bookTitle = books.data.data[i].volumeInfo.title
                 const bookAuthor = books.data.data[i].volumeInfo.authors
                 const bookPublisher = books.data.data[i].volumeInfo.publisher
                 const bookPublishedDate = books.data.data[i].volumeInfo.publishedDate
                 const bookThumbnail = books.data.data[i].volumeInfo.imageLinks.thumbnail
+
                 booksData(bookTitle, bookAuthor, bookPublisher, bookPublishedDate, bookThumbnail)
             }
-
-
         })
         .catch((error) => {
             console.error(`Error : ${error}`)
         })
 }
 
-getData()
-
-const booksContainer = document.querySelector('.booksContainer')
-
 function booksData(bookTitle, bookAuthor, bookPublisher, bookPublishedDate, bookThumbnail) {
-
     const books = document.createElement('div')
     const booksFooter = document.createElement('div')
     booksFooter.classList.add("booksFooter")
@@ -51,45 +45,32 @@ function booksData(bookTitle, bookAuthor, bookPublisher, bookPublishedDate, book
     booksFooter.append(publisher, pubDate)
     books.append(title, thumbnail, booksFooter)
     booksContainer.append(books)
-    
+
+    books.addEventListener('click', () => {
+        booksDetails()
+    })
+}
+function booksDetails() {
+
 }
 
 
-
 function booksView() {
-    const books = document.querySelectorAll('.books')
-    const booksFooter = document.querySelector('.booksFooter')
     if (listView.style.display === "block") {
         booksContainer.classList.add('active')
         listView.style.display = "none";
         gridView.style.display = "block";
-        }
-
-        else {
-            booksContainer.classList.remove('active')
-            gridView.style.display = "none"
-            listView.style.display = "block";
-        }
-   
-
     }
 
-function bookDetails(){
-    console.log("Kushal");
+    else {
+        booksContainer.classList.remove('active')
+        gridView.style.display = "none"
+        listView.style.display = "block";
+    }
+
+
 }
 
-
-
-    const books = document.querySelectorAll('.books')
-    console.log(books);
-    
-    books.forEach((book)=>{
-        book.addEventListener("click",()=>{
-            bookDetails()
-            
-        })
-    })
-
-
+getData()
 gridView.addEventListener('click', booksView)
 listView.addEventListener('click', booksView)
